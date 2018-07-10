13ab7ef92dc2f2658df448e3c598f27f6c9b76a2
Madong1994 <1170174224@qq.com>
2018-07-10 21:48:07 +0800

    cshi

---
+++ src/main/java/learn/Learn.java
@@ -0,0 +1,13 @@
+package learn;
+
+/**
+ * Created by 马东 on 2018/7/10.
+ *
+ * @Author:madong
+ * @Description:
+ * @Date:Create in 21:47 2018/7/10
+ * 关关雎鸠，在河之洲，
+ * 窈窕淑女，君子好逑。
+ */
+public class Learn {
+}
